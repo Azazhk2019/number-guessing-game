606807de69c3e50a4fa2bdb76ad19a00862eba9d
Azazhk2019 <azazhk2024@gmail.com>
2025-07-11 23:04:09 +0500

    Update number-guessing.js

--- Number Guessing Game/number-guessing.js
+++ Number Guessing Game/number-guessing.js
@@ -1,21 +1,3 @@
-// Question 1: Number Guessing Game (Beginner)
-// Problem:
-
-// The secret number is 5.
-
-// Ask the user to guess a number between 1 and 10.
-
-// The user has 3 chances.
-
-// If the guess is correct → Show "Correct!" and stop.
-
-// If the guess is wrong → Show "Try again!"
-
-// If all 3 chances are used → Show "Game over!"
-
-// Concepts Used:
-// while or for loop, if-else, break, user input
-
 let attempt = 0;
 while (attempt < 3){
     let userNumber = parseInt(prompt("Enter your guessing number from 1 to 10: "));
